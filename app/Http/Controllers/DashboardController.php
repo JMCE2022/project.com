1fcd0cbdd0c06cde2670c06780a512e1b3fe945d
HyTek <jmcegos@usep.edu.ph>
2024-01-03 00:28:46 +0800

    03/01/2024 12:28 am Pie Chart

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,23 +5,35 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Children;
+
 class DashboardController extends Controller
 {
     public function dashboard()
     {
+        $ageCount0 = Children::where('age', '=', 0)->count();
+        $ageCount1 = Children::where('age', '>=', 1)->where('age', '<=', 5)->count();
+        $ageCount2 = Children::where('age', '>=', 6)->where('age', '<=', 10)->count();
+        $ageCount3 = Children::where('age', '>=', 11)->where('age', '<=', 15)->count();
+        $ageCount4 = Children::where('age', '>=', 16)->where('age', '<=', 17)->count();
+        $ageCount5 = Children::where('age', '>=', 18)->count();
+
+        // Array containing age range labels and corresponding counts
+        $ageLabels = ['Unknown','1-5 Years old', '6-10 Years old', '11-15 Years old', '16-17 Years old', '18+ Years old'];
+        $ageCounts = [$ageCount0, $ageCount1, $ageCount2, $ageCount3, $ageCount4, $ageCount5];
+
         $childrenCount = Children::all()->count();
         $maleCount = Children::where('Sex', 'Male')->count();
-    $femaleCount = Children::where('Sex', 'Female')->count();
-    $dischargedCount = Children::where('is_deleted', '1')->count();
-   
+        $femaleCount = Children::where('Sex', 'Female')->count();
+        $dischargedCount = Children::where('is_deleted', '1')->count();
+
 
         if (Auth::user()->user_type == 'Admin') {
-            return view('admin.dashboard',compact('maleCount', 'femaleCount','dischargedCount','childrenCount'));
+            return view('admin.dashboard', compact('maleCount', 'femaleCount', 'dischargedCount', 'childrenCount','ageLabels', 'ageCounts'));
         } else if (Auth::user()->user_type == 'Staff') {
-            return view('staff.dashboard',compact('maleCount', 'femaleCount','dischargedCount','childrenCount'));
+            return view('staff.dashboard', compact('maleCount', 'femaleCount', 'dischargedCount', 'childrenCount','ageLabels', 'ageCounts'));
         }
     }
-    
-    
-    
+
+
+
 }
--- resources/views/Admin/dashboard.blade.php
+++ resources/views/Admin/dashboard.blade.php
@@ -41,54 +41,85 @@
                             <span class="fs-5 text-dark">CLIENT SERVED <i class="fas fa-hands-helping fa-1x"
                                     style="color: #000000;"></i> </span>
                         </div>
-                        <span class="fs-5 text-dark">{{  $childrenCount }}</span>
+                        <span class="fs-5 text-dark">{{ $childrenCount }}</span>
                     </div>
                 </div>
 
                 <div class="container-fluid pt-4 px-4">
-                <div class="row g-4">
-                    <div class="col-sm-12 col-xl-6">
-                        <div class="bg-secondary rounded h-100 p-4">
-                            <h6 class="mb-4">Single Line Chart</h6>
-                            <canvas id="line-chart"></canvas>
+                    <div class="row g-4">
+                        <div class="col-sm-12 col-xl-6">
+                            <div class="bg-secondary rounded h-100 p-4">
+                                <h6 class="mb-4">Single Line Chart</h6>
+                                <canvas id="line-chart"></canvas>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-sm-12 col-xl-6">
-                        <div class="bg-secondary rounded h-100 p-4">
-                            <h6 class="mb-4">Multiple Line Chart</h6>
-                            <canvas id="salse-revenue"></canvas>
+                        <div class="col-sm-12 col-xl-6">
+                            <div class="bg-secondary rounded h-100 p-4">
+                                <h6 class="mb-4">Multiple Line Chart</h6>
+                                <canvas id="salse-revenue"></canvas>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-sm-12 col-xl-6">
-                        <div class="bg-secondary rounded h-100 p-4">
-                            <h6 class="mb-4">Single Bar Chart</h6>
-                            <canvas id="bar-chart"></canvas>
+                        <div class="col-sm-12 col-xl-6">
+                            <div class="bg-secondary rounded h-100 p-4">
+                                <h6 class="mb-4">Single Bar Chart</h6>
+                                <canvas id="bar-chart"></canvas>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-sm-12 col-xl-6">
-                        <div class="bg-secondary rounded h-100 p-4">
-                            <h6 class="mb-4">Multiple Bar Chart</h6>
-                            <canvas id="worldwide-sales"></canvas>
+                        <div class="col-sm-12 col-xl-6">
+                            <div class="bg-secondary rounded h-100 p-4">
+                                <h6 class="mb-4">Multiple Bar Chart</h6>
+                                <canvas id="worldwide-sales"></canvas>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-sm-12 col-xl-6">
-                        <div class="bg-secondary rounded h-100 p-4">
-                            <h6 class="mb-4">Pie Chart</h6>
-                            <canvas id="pie-chart"></canvas>
+                        <div class="col-sm-12 col-xl-6">
+                            <div class="bg-secondary rounded h-100 p-4">
+                                <h6 class="mb-4">Pie Chart</h6>
+                                <canvas id="pie-chart" width="400" height="400"
+                                   ></canvas>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-sm-12 col-xl-6">
-                        <div class="bg-secondary rounded h-100 p-4">
-                            <h6 class="mb-4">Doughnut Chart</h6>
-                            <canvas id="doughnut-chart"></canvas>
+                        <div class="col-sm-12 col-xl-6">
+                            <div class="bg-secondary rounded h-100 p-4">
+                                <h6 class="mb-4">Doughnut Chart</h6>
+                                <canvas id="doughnut-chart"></canvas>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
 
             </div>
         </div>
     </div>
 </div>
+<script>
+        document.addEventListener("DOMContentLoaded", function () {
+            var ctx5 = $("#pie-chart").get(0).getContext("2d");
+
+            var ageLabels = {!! json_encode($ageLabels) !!};
+            var ageCounts = {!! json_encode($ageCounts) !!};
+
+            var myChart5 = new Chart(ctx5, {
+                type: "pie",
+                data: {
+                    labels: ageLabels,
+                    datasets: [{
+                        backgroundColor: [
+                            "rgba(235, 22, 22, 1)",
+                            "rgba(39, 226, 245, 0.8)",
+                            "rgba(39, 245, 84, 0.8)",
+                            "rgba(247, 255, 0, 0.8)",
+                            "rgba(255, 141, 0, 0.8)",
+                          
+                            "rgba(255, 0, 189, 0.8)"
+                        ],
+                        data: ageCounts
+                    }]
+                },
+                options: {
+                    responsive: true
+                }
+            });
+        });
+    </script>
 @endsection
